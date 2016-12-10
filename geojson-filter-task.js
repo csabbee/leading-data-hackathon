96f2a0871c92cd8783600d002ea0c423f6446691
ppseprus <p.seprus@gmail.com>
2016-12-10 05:08:52 +0100

    Remove json formatting

--- geojson-filter-task.js
+++ geojson-filter-task.js
@@ -13,6 +13,6 @@ gulp.task('filter-geojson', (done) => {
     telekomTowers.features = geojson.features.filter(feature => {
         return feature.properties.tags.operator && _.includes(feature.properties.tags.operator, 'Telekom');
     });
-    fs.writeFileSync('app/telekom_towers.geo.json', JSON.stringify(telekomTowers, null, '    '));
+    fs.writeFileSync('app/telekom_towers.geo.json', JSON.stringify(telekomTowers, null));
     done();
 });
